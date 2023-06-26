6b839c3069ac257f4520c7e41dc3ab142f7faed9
JGBV <46304804+jgbv@users.noreply.github.com>
2023-06-26 10:09:24 -0700

    Update scripts.js

--- prototypes/nlp/js/scripts.js
+++ prototypes/nlp/js/scripts.js
@@ -53,7 +53,7 @@ function parseQueryItems(){
         options: {
           title: {
             display: true,
-            text: "World Wide Wine Production 2018"
+            text: clientN
           }
         }
       });
